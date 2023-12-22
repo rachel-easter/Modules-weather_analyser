746f03933a6eb8e7f801db0dbd373365cb73549a
Rachel E <117534780+rachel-easter@users.noreply.github.com>
2023-12-22 16:38:54 +0530

    Update weatherApi.ts

--- src/weatherApi.ts
+++ src/weatherApi.ts
@@ -1,13 +1,13 @@
-// weatherApi.ts
+
 export async function fetchWeatherData(city: string): Promise<any> {
   try {
-    // Assuming that db.json is in the root directory of your project
-    const apiUrl = ' http://localhost:3000/weather';
+    
+    const apiUrl = 'db.json';
 
     const response = await fetch(apiUrl);
     const data = await response.json();
 
-    return data.weather; // Adjust to match the structure of your mock data
+    return data.weather; 
   } catch (error) {
     console.error('Error fetching weather data:', error);
     throw error;
